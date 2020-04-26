77616cea75b0ce8239d4e4ecb8d9ae9f52a2a3fd
valentincarrichon <valentincarrichon@outlook.com>
2020-04-26 20:38:00 +0200

    un petit css en plus et reorganisation des objets films

--- src/main.js
+++ src/main.js
@@ -13,9 +13,53 @@ Vue.use(BootstrapVue)
 window.shared_data = {
   mavariable: 'Test',
   films: [
-    {id:0, titre: 'stuart little', annee: 2002,langue:'EN', realisateur: {name:'Rob Minkoff',nationalite:'EN',birthday:'1962-08-11'}, synops: 'petite souris cours',genre:'Action', seen: false, note:1,poster:"https://static.lpnt.fr/images/2014/11/27/stuart-2951456-jpg_2581275_660x281.JPG" },
-    {id:1, titre: 'star wars', annee: 1997,langue:'FR', realisateur: {name:'Georges Lucas',nationalite:'EN',birthday:'1944-05-14'}, synops: 'vium vium',genre:'Action', seen: false, note:5,poster:"https://www.presse-citron.net/wordpress_prod/wp-content/uploads/2019/09/star-wars-ix-naissance-legendes-bataille-finale-epique.jpg" },
-    {id:2, titre: 'Spiderman', annee: 2002,langue:'FR', realisateur: {name:'Tom Holland',nationalite:'EN',birthday:'1996-06-01'}, synops: 'araignée mur',genre:'Action', seen : false, note:2,poster:"https://fr.web.img3.acsta.net/pictures/17/05/24/15/35/100661.jpg" },
+    {
+      id:0,
+      titre: 'Stuart little',
+      annee: 2002,langue:'EN',
+      realisateur: {
+        name:'Rob Minkoff',
+        nationalite:'EN',
+        birthday:'1962-08-11'
+      },
+      synops: 'Petite souris blanche marrante',
+      genre:'Action',
+      seen: false,
+      note:2,
+      poster:"https://static.lpnt.fr/images/2014/11/27/stuart-2951456-jpg_2581275_660x281.JPG"
+    },
+    {
+      id:1,
+      titre: 'star wars',
+      annee: 1997,
+      langue:'FR',
+      realisateur: {
+        name:'Georges Lucas',
+        nationalite:'EN',
+        birthday:'1944-05-14'
+      },
+      synops: 'Je suis ton père.',
+      genre:'Action',
+      seen: false,
+      note:3,
+      poster:"https://www.presse-citron.net/wordpress_prod/wp-content/uploads/2019/09/star-wars-ix-naissance-legendes-bataille-finale-epique.jpg"
+    },
+    {
+      id:2,
+      titre: 'Spiderman',
+      annee: 2002,
+      langue:'FR',
+      realisateur: {
+        name:'Tom Holland',
+        nationalite:'EN',
+        birthday:'1996-06-01'
+      },
+      synops: 'I believe I can fly.',
+      genre:'Action',
+      seen : false,
+      note:4,
+      poster:"https://fr.web.img3.acsta.net/pictures/17/05/24/15/35/100661.jpg"
+    },
 ],
 }
 
